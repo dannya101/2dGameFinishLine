fa35c3efb9b6deb03babbdd119f997e47ea7e522
dannya101 <124776050+dannya101@users.noreply.github.com>
2024-11-04 10:23:25 -0800

    Update cameraMove.cs

--- Assets/C#code/cameraMove.cs
+++ Assets/C#code/cameraMove.cs
@@ -15,15 +15,9 @@ public class cameraMove : MonoBehaviour
     public GameOver end;
     private bool isDead = false;
 
-    void Start()
-    {
-    }
+  
     void Update(){
         transform.position += Vector3.right * speed * Time.deltaTime;
-        // Vector3 screenPos = cam.WorldToScreenPoint(target.position);
-        // Vector3 targetPosition = target.position + offset;
-        // transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-        // Debug.Log(cam.pixelWidth);
         Vector3 bottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
         if(player.transform.position.x < bottomLeft.x - 3 && !isDead)
         {
