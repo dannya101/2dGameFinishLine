aab0acf0fbf5d358c4100bc88d57307c4d93b024
dannya101 <124776050+dannya101@users.noreply.github.com>
2024-11-04 10:29:56 -0800

    Update cameraMove.cs

--- Assets/C#code/cameraMove.cs
+++ Assets/C#code/cameraMove.cs
@@ -7,15 +7,14 @@ using Vector3 = UnityEngine.Vector3;
 
 public class cameraMove : MonoBehaviour
 {
-
-    // Start is called before the first frame update
     public float speed = 2.0f;
     public GameObject player;
     public Camera cam;
     public GameOver end;
     private bool isDead = false;
 
-  
+    //function to move the camera right with the product of how much time has elapsed, how fast the camera is suppose to move, and with the vector variable of the camera
+    //though there is a condition if the player gameobject is left of the view of the camera the game will end as a loss
     void Update(){
         transform.position += Vector3.right * speed * Time.deltaTime;
         Vector3 bottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
