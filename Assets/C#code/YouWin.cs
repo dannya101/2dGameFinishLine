ce61c6c60cb1f99f306051ba6aa0553db7adfcb2
dannya101 <124776050+dannya101@users.noreply.github.com>
2024-11-04 10:23:03 -0800

    Update YouWin.cs

--- Assets/C#code/YouWin.cs
+++ Assets/C#code/YouWin.cs
@@ -5,16 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class YouWin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     public void win()
     {
         SceneManager.LoadScene(0);
