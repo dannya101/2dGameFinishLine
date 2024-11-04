31fadd2af421bb6397ad5b1c741dd59991c1f260
dannya101 <124776050+dannya101@users.noreply.github.com>
2024-11-04 10:27:31 -0800

    Update YouWin.cs

--- Assets/C#code/YouWin.cs
+++ Assets/C#code/YouWin.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class YouWin : MonoBehaviour
 {
-    
+    //function to load the main menu screen
     public void win()
     {
         SceneManager.LoadScene(0);
     }
+    //collision function so once gameObject attached to you win is collied with a player tag object
+    //it will pause the game and show win screen
     public void OnCollisionEnter2D( Collision2D coll ) {
        GameObject collidedWith = coll.gameObject;
         if ( collidedWith.CompareTag("Player") ) {
